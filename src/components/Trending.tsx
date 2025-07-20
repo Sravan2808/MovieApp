387e9b8d915f89a411b956e352020ad2acc4a4b5
Sravan Surya <vsravansurya@gmail.com>
2025-07-20 11:30:44 +0530

    Trending page

--- src/components/Trending.tsx
+++ src/components/Trending.tsx
@@ -5,31 +5,48 @@ import DropDown from "./templates/DropDown";
 import axios from "../utils/axios";
 import Cards from "./templates/Cards";
 import Loading from "./templates/Loading";
+import InfiniteScroll from "react-infinite-scroll-component";
 
 const Trending = () => {
   const navigate = useNavigate();
   const [category, setcategory] = useState("all");
   const [duration, setduration] = useState("day");
-  const [trending, setTrending] = useState(null);
+  const [trending, setTrending] = useState([]);
+  const [page, setpage] = useState(1);
+  const [hasMore, setHasMore] = useState(true);
 
   const GetTrending = async () => {
     try {
-      const { data } = await axios.get(`/trending/${category}/${duration}`);
-      setTrending(data.results);
+      const { data } = await axios.get(`/trending/${category}/${duration}?page=${page}`);
+      // setTrending(data.results);
+      if (data.results.length > 0) {
+        setTrending((prevState) => [...prevState, ...data.results]);
+        setpage(page + 1);
+      } else {
+        setHasMore(false);
+      }
     } catch (error) {
       console.log("Error: ", error);
     }
   };
 
-  console.log(trending);
+  const refreshHandler = () => {
+    if (trending.length === 0) {
+      GetTrending();
+    } else {
+      setpage(1);
+      setTrending([]);
+      GetTrending();
+    }
+  };
 
   useEffect(() => {
-    GetTrending();
+    refreshHandler();
   }, [category, duration]);
 
-  return trending ? (
-    <div className="px-[3%] w-screen h-screen overflow-hidden overflow-y-auto">
-      <div className="w-full h-[10vh] flex items-center">
+  return trending.length > 0 ? (
+    <div className=" w-screen h-screen ">
+      <div className="px-[5%]w-full h-[10vh] flex items-center">
         <h1 className="text-2xl text-zinc-400 font-semibold">
           <i
             onClick={() => navigate(-1)}
@@ -37,7 +54,7 @@ const Trending = () => {
           ></i>
           Trending
         </h1>
-        <div className="w-[80%]">
+        <div className=" w-[80%]">
           <Topnav />
         </div>
         <DropDown
@@ -53,9 +70,18 @@ const Trending = () => {
         />
       </div>
 
-      <Cards data={trending} title={category} />
+      <InfiniteScroll
+        dataLength={trending.length}
+        next={GetTrending()}
+        hasMore={hasMore}
+        loader={<h1>Loading...</h1>}
+      >
+        <Cards data={trending} title={category} />
+      </InfiniteScroll>
     </div>
-  ):<Loading />;
+  ) : (
+    <Loading />
+  );
 };
 
 export default Trending;
--- src/components/templates/Cards.tsx
+++ src/components/templates/Cards.tsx
@@ -4,15 +4,15 @@ import { IMG_TMDB } from "../../utils/constants";
 
 const Cards = ({ data, title }) => {
   return (
-    <div className="flex flex-wrap w-[full]  pl-18 ">
-      {data.map((c) => (
-        <Link className="w-[25vh] mr-[5%] mb-[5%]" key={c.id}>
+    <div className="flex flex-wrap w-full h-full  pl-30 bg-[#1F1E24] ">
+      {data.map((c,id) => (
+        <Link className="w-[25vh] mr-[5%] mb-[5%]" key={id}>
           <img
             className="shadow-[8px_17px_38px_2px_rgba(0,0,0.5)] h-[40vh] object-cover"
             src={IMG_TMDB + (c.backdrop_path || c.profile_path)}
             alt=""
           />
-          <h1 className="text-2xl text-xinc-300 mt-3 font-semibold">
+          <h1 className="text-2xl text-zinc-300 mt-3 font-semibold">
             {c.name || c.title || c.original_name || c.original_title}
           </h1>
         </Link>
